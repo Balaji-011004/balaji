cc86ee770e4b38ba055fe29c679a9249a1561736
balaji <balajikottla2004@gmail.com>
2025-01-02 15:47:37 +0530

    hi

---
+++ src/main/java/com/kmit/maven1/App.java
@@ -0,0 +1,13 @@
+package com.kmit.maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "sum of 10 numbers is: 55" );
+    }
+}
